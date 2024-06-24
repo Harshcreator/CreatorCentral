60753a11d5400b0ff471e02677058abcd1aa525f
Harshcreator <jayson22603@gmai.com>
2024-06-24 10:30:16 +0530

    all setup config

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).
         catch((err) => next(err));
     }
@@ -11,7 +11,7 @@ const asyncHandler = (requestHandler) => {
 
 
 
-export {asysncHandler}
+export {asyncHandler}
 
 //const asysncHandler = (fn) => async (req, res, next) => {
 //    try {
